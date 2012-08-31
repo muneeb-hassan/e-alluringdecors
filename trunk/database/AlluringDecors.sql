368ce988ba5fe480e766b9e09067c7da3acd689b
lnbienit@gmail.com <lnbienit@gmail.com@63243daa-7d2e-388f-1df9-ef8380029976>
2012-08-31 09:38:16 +0000

    --sua ok

--- trunk/database/AlluringDecors.sql
+++ trunk/database/AlluringDecors.sql
@@ -15,15 +15,4 @@ CREATE TABLE Account(
 	Acting TINYINT NOT NULL DEFAULT 0
 );
 
-CREATE TABLE Acting(
-	ActingID INT IDENTITY(1,1) PRIMARY KEY,
-	Username VARCHAR(32) NOT NULL,
-	[View] BIT NOT NULL DEFAULT 0,
-	[Delete] BIT NOT NULL DEFAULT 0,
-	[Edit] BIT NOT NULL DEFAULT 0,
-	[Add] BIT NOT NULL DEFAULT 0
-);
-
-INSERT INTO Account(Username, [Password], FullName, Email, Acting) VALUES('admincp', '123456', N'Administrator', 'example@gmail.com', 2);
-INSERT INTO Acting(Username, [Add], [Delete], Edit, [View]) VALUES('admincp', 1, 1, 1, 1);
-45
+INSERT INTO Account(Username, [Password], FullName, Email, Acting) VALUES('admincp', '123456', N'Administrator', 'example@gmail.com', 1);
